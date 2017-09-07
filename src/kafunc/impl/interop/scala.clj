e825a7ad22418ae3cc5b80af924277ed92e02b75
Jason Graffius <jasongraffius@gmail.com>
2017-09-06 20:03:54 -0400

    Adding some Scala interop

---
+++ src/kafunc/impl/interop/scala.clj
@@ -0,0 +1,5 @@
+(ns kafunc.impl.interop.scala
+  "Scala-specific interop. These functions are used in the local Kafka server
+  related features only. Only the bare minimum is added here to allow for the
+  feature."
+  )
