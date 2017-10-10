09248ea44d62070c53cca004e4ae101a615ca493
Jason Graffius <jasongraffius@gmail.com>
2017-10-09 21:55:27 -0400

    Refactor, added local instance feature

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :url "https://github.com/kafunc/kafunc"
   :description "A less-imperative approach to Kafka."
   :dependencies [[org.apache.kafka/kafka_2.11 "0.10.2.1"]
+                 [org.apache.zookeeper/zookeeper "3.4.10"]
                  [org.clojure/clojure "1.8.0"]]
   :repl-options {:init-ns kafunc.core})
